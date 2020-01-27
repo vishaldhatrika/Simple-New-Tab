acd2d7f038c3c5ef9523470600c1984224cb9afa
Vechro <stenmr@gmail.com>
2020-01-27 23:50:54 +0200

    Formatting

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Set title
-document.title = chrome.i18n.getMessage('tabTitle');
+document.title = chrome.i18n.getMessage("tabTitle");
 
 // Set favicon
 function updateFavicon(e) {
-    let type = e.matches ? 'light' : 'dark';
-    document.querySelector('link[rel="shortcut icon"]').href = `icons/tab-${type}-32.png`;
+    const type = e.matches ? "light" : "dark";
+    document.querySelector("link[rel=\"shortcut icon\"]").href = `icons/tab-${type}-32.png`;
     chrome.runtime.sendMessage({ type: "update-icon" }); // Only works if this page is open, but still, better than nothing.
 }
-let mql = window.matchMedia('(prefers-color-scheme: dark)');
+const mql = window.matchMedia("(prefers-color-scheme: dark)");
 mql.onchange = updateFavicon;
 updateFavicon(mql);
