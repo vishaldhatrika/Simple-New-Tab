fa733d02a87683c400ad28e992a43f1d3d5b95b4
vechro <stenmr@gmail.com>
2020-02-05 15:10:49 +0200

    Small formatting changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Redirect to fakebox new tab
-chrome.tabs.update({ url: "chrome-search://local-ntp/local-ntp.html", active: true, selected: true })
+chrome.tabs.update({ url: "chrome-search://local-ntp/local-ntp.html", active: true, selected: true });
 
 // Set favicon
-function updateExtensionIcon(e) {
+function updateExtensionIcon() {
     chrome.runtime.sendMessage({ type: "update-icon" }); // Only works if this page is open, but still, better than nothing.
 }
 const mql = window.matchMedia("(prefers-color-scheme: dark)");
 mql.onchange = updateExtensionIcon;
-updateExtensionIcon(mql);
+updateExtensionIcon();
