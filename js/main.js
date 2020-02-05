7b9f2b93e3d9f70ae50de8e748e42d59a459451c
vechro <stenmr@gmail.com>
2020-02-05 15:03:49 +0200

    Remove unused variable

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ chrome.tabs.update({ url: "chrome-search://local-ntp/local-ntp.html", active: tr
 
 // Set favicon
 function updateExtensionIcon(e) {
-    const type = e.matches ? "light" : "dark";
     chrome.runtime.sendMessage({ type: "update-icon" }); // Only works if this page is open, but still, better than nothing.
 }
 const mql = window.matchMedia("(prefers-color-scheme: dark)");
